bb6dccd5a8240c40b2a193df97c77b1d864bea6d
saeedmuhammed <saidmsaid2011@gmail.com>
2023-04-19 07:25:15 +0200

    display and hide settings menu

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// show and hide settings menu
+let gear = document.querySelector(".settings .icon");
+gear.addEventListener("click",()=>{
+document.querySelector(".settings").classList.toggle("toggle");
+document.querySelector(".settings .icon i").classList.toggle("togglei");
+});
+
+
+
+
+
 // generate random images on landing page
 let landPage = document.querySelector(".land-page");
 let images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"]
-setInterval(function () {
+setInterval(() =>{
 
     let Random = Math.floor(Math.random() * images.length);
 
